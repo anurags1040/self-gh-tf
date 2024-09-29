ad61c0b3d195fe18b33731a75b4a9fdae08a1015
anurag-sharma_widwd <anurag.sharma@dwd.wisconsin.gov>
2024-09-29 13:51:47 -0700

    envs-final

--- terraform/modules/github/gh_config.tf
+++ terraform/modules/github/gh_config.tf
@@ -53,19 +53,11 @@ resource "github_repository_environment" "environments" {
     users = [for reviewer in each.value.reviewers : data.github_user.reviewer_ids[reviewer].id]
   }
 
-  # Set branch deployment policies
+  # Set branch deployment policies. custom_branch_policies and protected_branches settings cannot both have the
+  # same value, meaning that both cannot be true at the same time in the deployment_branch_policy of a GitHub environment.
   deployment_branch_policy {
     protected_branches     = each.value.branch_policies.protected_branches
     custom_branch_policies = each.value.branch_policies.protected_branches ? false : each.value.branch_policies.custom_branch_policies
   }
 }
-
-# Example of defining the repositories from a separate data or resource block
-# Assuming `github_repository.repos` references GitHub repositories to be created or imported elsewhere.
-# resource "github_repository" "repos" {
-#   for_each = var.github_config.repositories
-
-#   name = each.key
-#   # Add other repository configuration details here if necessary
-# }
 
