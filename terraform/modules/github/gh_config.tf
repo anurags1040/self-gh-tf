ca1049720f62ce8e12d53e39fd412d2dbb69f2dc
anurag-sharma_widwd <anurag.sharma@dwd.wisconsin.gov>
2024-09-29 13:46:37 -0700

    creating envs

--- terraform/modules/github/gh_config.tf
+++ terraform/modules/github/gh_config.tf
@@ -1,13 +1,24 @@
-#Reference existing repos instead of creating new ones
-
-data "github_repository" "repo" {
-  for_each = var.github_config.repositories
-
-  name = each.key
-}
-
 #user id error for reviewers. The following data block converts the github username to github user id which
 #is the accepted format here.
+
+# Check if repositories already exist using a data source
+data "github_repository" "existing_repos" {
+  for_each = var.github_config.repositories
+  name     = each.key
+}
+
+# Create repositories only if they do not exist
+resource "github_repository" "repos" {
+  for_each = {
+    for repo, repo_config in var.github_config.repositories :
+    repo => repo_config
+    if try(data.github_repository.existing_repos[repo].id, null) == null  # Only include repositories that don't exist
+  }
+
+  name = each.key
+
+  # Add other repository configuration details here if necessary
+}
 
 # Lookup GitHub user IDs based on usernames
 data "github_user" "reviewer_ids" {
@@ -35,7 +46,7 @@ resource "github_repository_environment" "environments" {
     }
   ]...)
 
-  repository  = data.github_repository.repos[each.value.repository].name
+  repository  = try(github_repository.repos[each.value.repository].name, data.github_repository.existing_repos[each.value.repository].name)
   environment = each.value.environment
 # Use the reviewers provided in the github_config
   reviewers {
@@ -45,16 +56,16 @@ resource "github_repository_environment" "environments" {
   # Set branch deployment policies
   deployment_branch_policy {
     protected_branches     = each.value.branch_policies.protected_branches
-    custom_branch_policies = each.value.branch_policies.custom_branch_policies
+    custom_branch_policies = each.value.branch_policies.protected_branches ? false : each.value.branch_policies.custom_branch_policies
   }
 }
 
 # Example of defining the repositories from a separate data or resource block
 # Assuming `github_repository.repos` references GitHub repositories to be created or imported elsewhere.
-resource "github_repository" "repos" {
-  for_each = var.github_config.repositories
+# resource "github_repository" "repos" {
+#   for_each = var.github_config.repositories
 
-  name = each.key
-  # Add other repository configuration details here if necessary
-}
+#   name = each.key
+#   # Add other repository configuration details here if necessary
+# }
 
